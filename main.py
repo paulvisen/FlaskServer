541f70a85381457cc811b0791078c2eb3d9a1030
paulvisen <zhvisen@gmail.com>
2016-05-23 19:51:28 +0800

    之前的代码重新上传

---
+++ main.py
@@ -0,0 +1,34 @@
+#-*- coding: UTF-8 -*- 
+
+""""from falsk ,jsonfiy,json"""
+
+
+
+
+from flask import Flask
+from flask import session
+from flask import redirect
+from models import *
+from hashmd5 import *
+from checkpage import check_page
+from devicelist import device_list
+from historydata import history_data
+from scenetask import scenetask
+
+import os,stat
+import shutil
+import string
+
+app=create_app()
+
+##注册蓝本路由
+
+app.register_blueprint(check_page)    #注册与登录
+app.register_blueprint(device_list)   #获取设备列表
+app.register_blueprint(history_data)  #获取设备历史信息
+app.register_blueprint(scenetask)  #实时任务列表操作
+
+   
+    
+if __name__=='__main__':
+    app.run(host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT',8080))
